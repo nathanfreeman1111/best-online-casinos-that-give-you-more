c07c5dd09a1ca1d2209445bc34888a21be3043ef
nathanfreeman1111 <nathan.freeman9401@gmail.com>
2022-11-05 13:03:18 +0530

    initial commit

---
+++ spec/best-online-casinos-that-give-you-more-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BestOnlineCasinosThatGiveYouMoreView from '../lib/best-online-casinos-that-give-you-more-view';
+
+describe('BestOnlineCasinosThatGiveYouMoreView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
